7e17a4aec704d4e3364809bfaa290c87af735fdb
yuttm <l0965166503@gmail.com>
2022-06-21 18:58:39 +0800

    20220621 add tk window

--- class1/20220602.py
+++ class1/20220602.py
@@ -1,3 +1,5 @@
+import turtle
+
 print("hello")
 print(2**8)
 print("2**8")
--- class5/20220616.py
+++ class5/20220616.py
@@ -12,17 +12,39 @@
 #     else:
 #         print("erro")
 
-drink = ["頻果汁", "柳橙汁", "葡萄汁", "離開系統"]
-while True:
-    print("1頻果汁\n2柳橙汁\n3葡萄汁\n4離開系統")
-    ans = int(input("請輸入"))
-    if ans == 4:
-        break
-    elif ans > len(drink):
-        print('error')
-    else:
-        print(drink[ans - 1])
+# drink = ["頻果汁", "柳橙汁", "葡萄汁", "離開系統"]
+# while True:
+#     print("1頻果汁\n2柳橙汁\n3葡萄汁\n4離開系統")
+#     ans = int(input("請輸入"))
+#     if ans == 4:
+#         break
+#     elif ans > len(drink):
+#         print('error')
+#     else:
+#         print(drink[ans - 1])
+#append新增元素到字串最後
+#insert在指定位置插入指定元素
+#pop()移除串列裡的最最後一個
+#sort由小至大排序串列元素
+#reverse顛倒串列順序
+#l[1,2,3,]
+#index+l.index("1")
 
-    drink[1] = "柳橙汁"
-    drink[2] = "葡萄汁"
-    drink[3]
+juice = []
+while True:
+    print("1輸入想要的果汁\2顯示目前的果汁\3離開系統")
+    x = input("輸入功能變換")
+    if x == "1":
+        j = int(input("輸入想要的果汁"))
+        for i in juice:
+            if i == j:
+                print("此果汁重複")
+                break
+        else:
+            juice.append(j)
+    elif x == "2":
+        print(juice)
+    elif x == "3":
+        break
+    else:
+        print("查無東西")
